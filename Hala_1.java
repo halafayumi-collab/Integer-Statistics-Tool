2b95dfcba0008d4798ca535caee24c3b2cff4b84
halafayumi-collab <halafayumi@gmail.com>
2026-01-02 02:23:48 +0300

    Initial commit

---
+++ Hala_1.java
@@ -0,0 +1,39 @@
+
+
+package hala_1;
+import java.util.*;
+public class Hala_1 {
+
+  public static void main(String[]args){
+      Scanner input=new Scanner(System.in);
+      System.out.println("enter an integer,the input ends if it`s 0 : ");
+      int number =input.nextInt();
+      int pos=0;
+      int neg=0;
+      double total=0;
+      if (number==0){
+          System.out.println("no nuumbers are entered except 0 ");
+          System.exit(0);
+      }
+      while (number!=0){
+      total+=number;
+      if(number>0)
+          pos++;
+      else neg++;
+      number=input.nextInt();
+      }
+      System.out.println("the number of positive is "+pos);
+      System.out.println("the number of negative is "+neg);
+      System.out.println("the the total is "+total);
+      double average=total/(pos+neg);
+      System.out.println("the average is "+average);
+      }
+     public static int sumdigit (long n ){
+int sum=0;
+ while (n!=0){
+ sum+=n%10;
+ n=n/10;
+ } return sum;
+}
+
+}
